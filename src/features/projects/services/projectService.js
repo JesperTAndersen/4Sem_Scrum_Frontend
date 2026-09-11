310eea55c2a10546aa86fa906b427f020a011dd8
Morten Jensen <mo_ten@hotmail.com>
2026-09-11 10:59:09 +0200

    service: added service implementation for projects api contract

---
+++ src/features/projects/services/projectService.js
@@ -0,0 +1,39 @@
+import apiClient from "@/api/apiclient";
+
+const RESOURCE = "/projects";
+
+const getAll = async () => {
+  return await apiClient(RESOURCE);
+};
+
+const getById = async (id) => {
+  return await apiClient(`${RESOURCE}/${id}`);
+};
+
+const update = async (id, body) => {
+  return await apiClient(`${RESOURCE}/${id}`, {
+    method: "PUT",
+    body: body,
+  });
+};
+
+const create = async (body) => {
+  return await apiClient(RESOURCE, {
+    method: 'POST',
+    body: body,
+  });
+};
+
+const remove = async (id) => {
+  return await apiClient(`${RESOURCE}/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export default {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+};
