ca007841bc2236881159264e6bd0d62f14ec83df
Morten Jensen <mo_ten@hotmail.com>
2026-09-20 16:36:53 +0200

    feat: Connected CompentenceManagmentPage in routes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -29,7 +29,7 @@ const AppRoutes = () => (
         <Route path="/projects/:id" element={<ProjectDetailPage />} />
         <Route path="/stages" element={<UnderDevelopmentPage />} />
         <Route path="/tasks" element={<UnderDevelopmentPage />} />
-        <Route path="/competences" element={<UnderDevelopmentPage />} />
+        <Route path="/competences" element={<CompetenceManagementPage />} />
         <Route path="/costs" element={<UnderDevelopmentPage />} />
         <Route path="/users" element={<UserManagementPage />} />
         <Route path="/profile" element={<UserProfilePage />} />
